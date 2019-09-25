4b56fc57d3f28786c718373e7a7fc5f207b17944
Eug-VS <eug-vs@keemail.me>
2019-09-25 14:44:23 +0300

    Implement find_slice() function
    -Rename "shapes" -> "blocks"
    -Add Shape.__repr__()

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-from toolkit import Shape, random_shape
+from toolkit import *
 
 M = 4
 N = 5
 
 surface = Shape(M, N)
-shapes = [Shape(3, 2), Shape(2, 2), Shape(2, 1), Shape(2, 3), Shape(1, 2)]
+blocks = [Shape(3, 2), Shape(2, 2), Shape(2, 1), Shape(2, 3), Shape(1, 2)]
 
 
 def print_task():
     print("Surface:")
     print("\t", surface)
     print("Blocks: ")
-    for shape in shapes:
-        print("\t", shape)
+    for block in blocks:
+        print("\t", block)
 
 
 print_task()
--- toolkit.py
+++ toolkit.py
@@ -11,9 +11,38 @@ class Shape:
     def __str__(self):
         return f'Shape({self.h} x {self.w})'
 
+    def __repr__(self):
+        return self.__str__()
+
     def area(self):
         return self.w * self.h
 
 
 def random_shape(shape):
     return Shape(randint(1, shape.h), randint(1, shape.w))
+
+
+def find_slice(surface, blocks, orientation):
+    results = []
+    for block in blocks:
+        for other in blocks:
+            if other != block:
+                if orientation == "horizontal":
+                    if block.h + other.h == surface.h:
+                        results.append((block, other))
+                elif orientation == "vertical":
+                    if block.w + other.w == surface.w:
+                        results.append((block, other))
+    for pair in results:
+        for other in results:
+            if other == (pair[1], pair[0]):
+                results.remove(other)
+    return results
+
+
+def find_slice_horizontal(surface, blocks):
+    return find_slice(surface, blocks, "horizontal")
+
+
+def find_slice_vertical(surface, blocks):
+    return find_slice(surface, blocks, "vertical")
