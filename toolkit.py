2e9cb36b31261a03bd163bf7a7d141aa104a76cb
Eug-VS <eug-vs@keemail.me>
2019-09-25 13:35:39 +0300

    Add Shape.area() method

--- toolkit.py
+++ toolkit.py
@@ -2,7 +2,9 @@ class Shape:
     def __init__(self, h, w):
         self.w = w
         self.h = h
-        self.area = self.w * self.h
-
+    
     def __str__(self):
         return f'Shape({self.h} x {self.w})'
+
+    def area(self):
+        return self.w * self.h
